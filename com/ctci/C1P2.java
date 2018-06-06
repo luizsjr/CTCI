88132e77e977849f2b341467ce1c2ba7ad3f47fe
Luiz <luizsjr@gmail.com>
2018-06-06 01:28:52 -0700

    Problems 1.2 and 1.3

---
+++ com/ctci/C1P2.java
@@ -0,0 +1,97 @@
+package com.ctci;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class C1P2 {
+
+	public static void main(String[] args) {
+		C1P2 problem = new C1P2();
+		
+		String strA = "abcc";
+		String strB = "cbca";
+		
+		System.out.println( problem.arePermutations(strA, strB) );
+		System.out.println( problem.arePermutations2(strA, strB) );
+		System.out.println( problem.arePermutations3(strA, strB) );
+
+	}
+	
+	//O(n log n) - not as optimal, but cleaner (preferable)
+	public boolean arePermutations(String strA, String strB) {
+		
+		if(strA.length() != strB.length()) {
+			return false;
+		}
+		
+		char[] charsA = strA.toCharArray();
+		char[] charsB = strB.toCharArray();
+		
+		Arrays.sort(charsA);
+		Arrays.sort(charsB);
+		
+		for(int i=0; i<charsA.length; i++) {
+			if (charsA[i] != charsB[i]) return false;
+		}
+		return true;
+	}
+	
+	// O(n) - optimal but not cleaner - HashMap implementation
+	public boolean arePermutations2(String strA, String strB) {
+		
+		if(strA.length() != strB.length()) {
+			return false;
+		}
+		
+		HashMap<Character, Integer> control = new HashMap<>();
+		
+		for (int i=0; i < strA.length(); i++) {
+			char c = strA.charAt(i);
+			//c = Character.toLowerCase(c); // if not case sensitive
+			Integer counter = control.get(c);
+			if(counter == null) counter = 0;
+			control.put(c, counter+1);
+		}
+		
+		for (int i=0; i < strB.length(); i++) {
+			char c = strB.charAt(i);
+			//c = Character.toLowerCase(c); // if not case sensitive
+			Integer counter = control.get(c);
+			if(counter == null || counter == 0) {
+				return false;
+			}
+			control.put(c, counter-1);
+		}
+		
+		//Not needed as lengths are equal
+//		for(Integer counter : control.values()) {
+//			if(counter > 0) {
+//				return false;
+//			}
+//		}
+		
+		return true;
+	}
+	
+	// O(n) - optimal but not cleaner - array implementation (better than 2, but 1 is still preferable)
+	public boolean arePermutations3(String strA, String strB) {
+		
+		if(strA.length() != strB.length()) {
+			return false;
+		}
+		
+		int[] letters = new int[128]; //assuming char set is ASCII
+		
+		for (int i=0; i < strA.length(); i++) {
+			char c = strA.charAt(i);
+			letters[c]++;
+		}
+		
+		for (int i=0; i < strB.length(); i++) {
+			char c = strB.charAt(i);
+			letters[c]--;
+			if (letters[c]<0) return false;
+		}
+		return true;
+	}
+}
---
+++ com/ctci/C1P3.java
@@ -0,0 +1,32 @@
+package com.ctci;
+
+public class C1P3 {
+
+	public static void main(String[] args) {
+		C1P3 problem = new C1P3();
+		
+		char[] str = "Mr John Smith    ".toCharArray();
+		int realSize = 13;
+		System.out.println(problem.urlify(str, realSize));
+
+	}
+	
+	public char[] urlify(char[] str, int realSize) {
+		int bufferSize = str.length - realSize;
+		
+		if (bufferSize==0) return str;
+		
+		for (int i=str.length-bufferSize-1; i>=0; i--) {
+			if (str[i] != ' ') {
+				str[i+bufferSize] = str[i];
+			} else {
+				str[i+bufferSize--] = '0';
+				str[i+bufferSize--] = '2';
+				str[i+bufferSize] = '%';
+				if (bufferSize==0) break;
+			}
+		}
+		return str;
+	}
+
+}
