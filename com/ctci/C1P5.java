25ca3a64dbe162d0bd4af32cbb55bcf5d271ff6f
Luiz <luizsjr@gmail.com>
2018-06-07 01:31:03 -0700

    Problems 1.5 and 1.6

---
+++ com/ctci/C1P5.java
@@ -0,0 +1,66 @@
+package com.ctci;
+
+public class C1P5 {
+
+	public static void main(String[] args) {
+		String strA = "pale";
+		String strB = "pales";
+		
+		C1P5 problem = new C1P5();
+		System.out.println(problem.isOneAway(strA, strB));
+		System.out.println(problem.isOneAway2(strA, strB));
+	}
+	
+	// O(n) where n is the shorter size - shorter but more complex
+	public boolean isOneAway(String strA, String strB) {
+		int diffSize = strA.length() - strB.length();
+		if (diffSize < -1 || diffSize > 1) return false;
+		
+		String bigger = diffSize < 0 ? strB : strA;
+		String smaller = diffSize < 0 ? strA : strB;
+		
+		int diffCounter = 0;
+		int offSet = 0;
+		
+		for (int i=0; i<smaller.length() && (i+offSet)<bigger.length(); i++) {
+			if (smaller.charAt(i) != bigger.charAt(i+offSet)) {
+				if (++diffCounter > 1) return false;
+				if (diffSize != 0) offSet++;
+			}
+		}
+		return true;
+	}
+	
+	// O(n) - longer, easier to understand
+	public boolean isOneAway2(String strA, String strB) {
+		int diffSize = strA.length() - strB.length();
+		
+		if (diffSize==0) 
+			return isOneEditAway(strA, strB);
+		
+		if (diffSize==1) 
+			return isOneInsertAway(strA, strB);
+		
+		if (diffSize==-1) 
+			return isOneInsertAway(strB, strA);
+		
+		return false;
+		
+	}
+	
+	public boolean isOneEditAway(String strA, String strB) {
+		int diffCounter = 0;
+		for (int i=0; i<strA.length(); i++) {
+			if(strA.charAt(i) != strB.charAt(i) && ++diffCounter > 1) return false;
+		}
+		return true;
+	}
+	
+	public boolean isOneInsertAway(String bigger, String smaller) {
+		int offSet = 0;
+		for (int i=0; i<smaller.length(); i++) {
+			if (smaller.charAt(i) != bigger.charAt(i+offSet) && ++offSet > 1) return false; 
+		}
+		return true;
+	}
+}
---
+++ com/ctci/C1P6.java
@@ -0,0 +1,29 @@
+package com.ctci;
+
+public class C1P6 {
+
+	public static void main(String[] args) {
+		C1P6 problem = new C1P6();
+		
+//		String str = "aaabbccccccdde";
+		String str = "aabbcccddee";
+		System.out.println(problem.compressString2(str));
+	}
+	
+	public String compressString2(String str) {
+		
+		if (str.length() < 3) return str;
+
+		StringBuilder compressed = new StringBuilder();
+		int charCounter=0;
+		for (int i=0; i<str.length(); i++) {
+			charCounter++;
+			if (i+1 == str.length() || str.charAt(i) != str.charAt(i+1)) {
+				compressed.append(str.charAt(i)).append(charCounter);
+				charCounter = 0;
+			}
+		}
+		
+		return compressed.length() < str.length() ? compressed.toString() : str;
+	}
+}
