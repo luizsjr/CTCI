4fc529366bb8c1a97883832f167b9d80c8082c4a
Luiz <luizsjr@gmail.com>
2018-06-08 00:26:00 -0700

    Problem 1.9

---
+++ com/ctci/C1P9.java
@@ -0,0 +1,22 @@
+package com.ctci;
+
+public class C1P9 {
+
+	public static void main(String[] args) {
+		
+		String s1 = "waterwaster";
+		String s2 = "wasterwater";
+		C1P9 problem = new C1P9();
+		System.out.println(problem.isRotation(s1, s2));
+	}
+	
+	// Assuming case sensitive
+	// O(n)
+	public boolean isRotation(String s1, String s2) {
+		if (s1 != null && s2 != null && s1.length() == s2.length() && s1.length() > 0) {
+			String s1s1 = s1 + s1;
+			return s1s1.indexOf(s2) > 0; // equivalent to the isSubstring
+		}
+		return false;
+	}
+}
