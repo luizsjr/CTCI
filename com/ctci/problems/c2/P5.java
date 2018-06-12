7ed1757c9b897b3d556375f9d71c9d52c37ac66c
Luiz Souza <luiz.l.souza@ericsson.com>
2018-06-11 17:15:09 -0700

    Problem 2.5 - Recursive solution

--- com/ctci/problems/c2/P5.java
+++ com/ctci/problems/c2/P5.java
@@ -23,12 +23,31 @@ public class P5 {
 		}
 		return head;
 	}
+	
+	public ListNode<Integer> sumLists(ListNode<Integer> l1, ListNode<Integer> l2, int carryOver) {
+		//Base Case
+		if(l1==null && l2==null && carryOver==0) return null;
+		
+		int sum = carryOver;
+		if(l1!=null) sum += l1.getData();
+		if(l2!=null) sum += l2.getData();
+		
+		ListNode<Integer> result = new ListNode<Integer>(sum%10);
+		
+		ListNode<Integer> next = sumLists( l1 == null ? null : l1.getNext(),
+											l2 == null ? null : l2.getNext(),
+											sum / 10 );
+		
+		result.setNext(next);
+		
+		return result;
+	}
 
 	public static void main(String[] args) {
 		ListNode<Integer> l1 = new ListNode<>(7,1,6);
 		ListNode<Integer> l2 = new ListNode<>(5,9,3);
 		P5 problem = new P5();
-		l1 = problem.sumLists(l1, l2);
+		l1 = problem.sumLists(l1, l2, 0);
 		System.out.println(l1.getListNodeString());
 		
 
