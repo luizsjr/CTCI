fb8ae297b6e6752f7f113dfafdf06e5078408905
Luiz de Souza <luiz.l.souza@ericsson.com>
2018-06-27 16:44:05 -0700

    Problem 4.9

--- com/ctci/problems/c4/P9.java
+++ com/ctci/problems/c4/P9.java
@@ -9,82 +9,64 @@ public class P9 {
 	
 	public List<List<BinaryTreeNode<Integer>>> findArrays(BinaryTreeNode<Integer> root) {
 		List<List<BinaryTreeNode<Integer>>> possibilities = new ArrayList<>();
-		return findArrays(possibilities, null, root);
+		findArrays(possibilities, null, root);
+		return possibilities;
 	}
 			
-	protected List<List<BinaryTreeNode<Integer>>> findArrays(
+	protected void findArrays(
 			List<List<BinaryTreeNode<Integer>>> possibilities, 
 			BinaryTreeNode<Integer> parent, 
 			BinaryTreeNode<Integer> root) {
 		
-		if (root==null) { return possibilities; }
-		possibilities=blendUntilNode(possibilities, parent, root);
-		possibilities = findArrays(possibilities, root, root.left);
-		possibilities = findArrays(possibilities, root, root.right);
-		return possibilities;
+		if (root==null) { return; }
+		blendUntilNode(possibilities, parent, root);
+		findArrays(possibilities, root, root.left);
+		findArrays(possibilities, root, root.right);
 	}
 	
-	protected List<List<BinaryTreeNode<Integer>>> blendUntilNode( 
-			List<List<BinaryTreeNode<Integer>>> listOfLists, 
-			BinaryTreeNode<Integer> limit, 
+	protected void blendUntilNode( 
+			List<List<BinaryTreeNode<Integer>>> combinations, 
+			BinaryTreeNode<Integer> start, 
 			BinaryTreeNode<Integer> newNode ) {
 		
-		List<List<BinaryTreeNode<Integer>>> result = new ArrayList<>();
-		for (List<BinaryTreeNode<Integer>> nodeList : listOfLists) {
-			
-			if(nodeList.size()<1) {
-				// 0 or 1 elements, add the new element in the end
-				List<BinaryTreeNode<Integer>> newCombination = new ArrayList<>();
-				newCombination.addAll(nodeList);
-				newCombination.add(newNode);
-				result.add(newCombination);
-			}
-			
-			// Check if nodeUntil belongs to the list
-			int blendIndex = nodeList.lastIndexOf(limit);
-			if (blendIndex < 0) {
-				throw new RuntimeException("untilNode parameter not in the list: "+limit);
-			}
-			
-			// blend
-			while (blendIndex < nodeList.size()) {
+		// Combinations list is empty, create combination to include the newNode
+		if(combinations.isEmpty()) {
+			List<BinaryTreeNode<Integer>> newCombination = new ArrayList<>();
+			combinations.add(newCombination);
+		}
+		
+		// The original combinations size need to be used otherwise we'll end up in an infinite loop 
+		int combineLimitIndex=combinations.size();
+		int currentIndex=0;
+		// For each existing combination, generate new combinations blending newNode in each combination
+		while (currentIndex < combineLimitIndex) {
+			List<BinaryTreeNode<Integer>> nodeList = combinations.get(currentIndex++);
+			// get the start node index (we cannot blend newNode before the parent node being included)
+			int blendIndex = nodeList.lastIndexOf(start);
+			// blend the newNode in the current combination
+			// we need to stop at end node - 1 as we are going to include in newNode in the end of the current combination
+			while (blendIndex < nodeList.size()-1) {
 				List<BinaryTreeNode<Integer>> newCombination = new ArrayList<>();
 				newCombination.addAll(nodeList);
 				newCombination.add(++blendIndex, newNode);
-				result.add(newCombination);
+				combinations.add(newCombination);
 			}
+			// Add the newNode in the current combination. (That's why we stopped in size-1 while generating new combinations )
+			nodeList.add(newNode);
 		}
-		return result;
 	}
 	
 	public static void main(String[] args) {
 		P9 problem = new P9();
 		
-		List<List<BinaryTreeNode<Integer>>> listOfLists = new ArrayList<>();
-		List<BinaryTreeNode<Integer>> nodesList = new ArrayList<>();
-		listOfLists.add(nodesList);
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<>(10);
+		root.left = new BinaryTreeNode<>(5);
+		root.right = new BinaryTreeNode<>(20);
 		
-		BinaryTreeNode<Integer> node = new BinaryTreeNode<>(10);
-		nodesList.add(node);
+		root.left.left= new BinaryTreeNode<>(3);
+		root.left.right= new BinaryTreeNode<>(6);
 		
-		BinaryTreeNode<Integer> node2 = new BinaryTreeNode<>(5);
-		nodesList.add(node2);
-		
-		BinaryTreeNode<Integer> node3 = new BinaryTreeNode<>(20);
-		nodesList.add(node3);
-		 
-		nodesList = new ArrayList<>();
-		nodesList.add(node);
-		nodesList.add(node3);
-		nodesList.add(node2);
-		listOfLists.add(nodesList);
-		
-		 System.out.println(listOfLists);
-		
-		BinaryTreeNode<Integer> newNode = new BinaryTreeNode<>(3);
-		List<List<BinaryTreeNode<Integer>>> result = problem.blendUntilNode(listOfLists, node, newNode);
-		
-		System.out.println(result);
+		System.out.println(problem.findArrays(root));
 	}
 
 }
