8bd08da21775f715dfc4e615b0228c9999900224
Luiz <luizsjr@gmail.com>
2018-07-03 22:30:33 -0700

    Problem 5.3

--- com/ctci/problems/c5/P3.java
+++ com/ctci/problems/c5/P3.java
@@ -3,13 +3,31 @@ package com.ctci.problems.c5;
 public class P3 {
 	public static void main(String[] args) {
 		P3 problem = new P3();
-		System.out.println(problem.isBitEnable(5, 4));
+		System.out.println(problem.flipBitToWin(-142606282)); // ‭11110111100001010100011111000110‬ -> 9
+		System.out.println(problem.flipBitToWin(-8388554)); //‭11111111100000000000000000110110‬ -> 10
+		System.out.println(problem.flipBitToWin(-69205962)); //‭11111011111000000000000000110110 -> 11‬
+		System.out.println(problem.flipBitToWin(1772)); //‭ ‭011011101100‬ -> 6‬
+		System.out.println(problem.flipBitToWin(-1)); //‭ ‭11111111111111111111111111111111‬ -> 32‬
+		System.out.println(problem.flipBitToWin(-1073741824)); //‭ ‭11000000000000000000000000000000‬ -> 3‬
 	}
 	
-	protected boolean isBitEnable(int n, int bitNumber) {
-		int mask = 1 << bitNumber-1;
-		System.out.println(mask);
-		return (n & mask) != 0;
+	public int flipBitToWin(int n) {
+		if (n==-1) return Integer.BYTES * 8;
+		int current=0;
+		int previous=0;
+		int max=0;
+		while(n!=0) {
+			// first bit of n is enabled, increment the current counter
+			if ((n&1)==1) {  
+				current++;
+			// otherwise sum with previous sequence + 1 (the bit being flipped) and compare with the max
+			} else {
+				max=Math.max(previous+1+current, max);
+				previous=current;
+				current=0;
+			}
+			n>>>=1;
+		}
+		return Math.max(previous+1+current, max);
 	}
-
 }
