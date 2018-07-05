abcc0617d97c5bc7e7bdf829faf143e875bfb326
Luiz de Souza <luiz.l.souza@ericsson.com>
2018-07-05 16:53:55 -0700

    Problem 5.6 optimization

--- com/ctci/problems/c5/P6.java
+++ com/ctci/problems/c5/P6.java
@@ -11,6 +11,14 @@ public class P6 {
 		}
 		return result;
 	}
+	
+	public int bitsDiffOptimized(int a, int b) {
+		int counter=0;
+		for(int i=a^b;i!=0;i=i&i-1) { // i & i-1 always clear the last bit set to 1
+			counter++;
+		}
+		return counter;
+	}
 
 	public static void main(String[] args) {
 		P6 problem = new P6();
@@ -18,6 +26,11 @@ public class P6 {
 		System.out.println(problem.bitsDiff(-1, 0)); //32
 		System.out.println(problem.bitsDiff(-1, 1)); //31
 		System.out.println(problem.bitsDiff(-2, 7)); //30
+		System.out.println("============================");
+		System.out.println(problem.bitsDiffOptimized(29, 15)); //2
+		System.out.println(problem.bitsDiffOptimized(-1, 0)); //32
+		System.out.println(problem.bitsDiffOptimized(-1, 1)); //31
+		System.out.println(problem.bitsDiffOptimized(-2, 7)); //30
 	}
 
 }
